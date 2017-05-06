76ffe545f232f1d5dd9c4826ff87d8797631dff5
Sander <sander@funkhaus.us>
2017-05-06 16:37:59 -0400

    Added lodash get to router/registry

--- src/router/registry.js
+++ src/router/registry.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import frontPage from 'src/components/templates/front-page.vue'
 import about from 'src/components/templates/about.vue'
+import _ from 'lodash'
 
 // Any state on the server gets its own template
 Vue.component( 'front-page-template', frontPage )
@@ -10,8 +11,7 @@ export default {
     template: `<component :is="cmpState"></component>`,
     computed: {
         cmpState(){
-            // TODO: Switch to _.get ?
-            return this.$store.state.queryData.state + '-template'
+            return _.get(this.$store, 'state.queryData.state') + '-template'
         }
     }
 }
