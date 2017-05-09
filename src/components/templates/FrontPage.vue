e16b0f9a5a765c10e77b44e4c4e6ff7f15be5f4c
Sander <sander@funkhaus.us>
2017-05-09 15:23:23 -0400

    Removed extra code from FrontPage template

--- src/components/templates/FrontPage.vue
+++ src/components/templates/FrontPage.vue
@@ -1,28 +1,5 @@
 <template>
 
-    <section class="front-page">
-
-        <router-link to="/about">About</router-link>
-
-        <p v-html="content"></p>
-    </section>
+    <!-- Front page -->
 
 </template>
-
-<script>
-import _ from 'lodash'
-
-export default {
-    computed: {
-        content(){
-            const pageData = _.get(this.$store, 'state.queryData.data[0].content')
-            return pageData
-        }
-    }
-}
-
-</script>
-
-<style>
-
-</style>
