2505d3839dd87223d163c3779fbbaa87dac5ffb6
Sander Moolin <sandermoolin@gmail.com>
2017-08-08 10:33:47 -0400

    Removed maximum-scale declaration to increase accessibility

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <?php if( is_front_page() ) : ?>
         <meta name="description" content="<?php bloginfo('description'); ?>">
     <?php endif; ?>
