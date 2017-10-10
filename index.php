37f4d28f0011e16126f5914ba4566de8bc1f9166
Sander Moolin <sandermoolin@gmail.com>
2017-10-10 16:27:10 -0400

    Added webfontloader to head

--- index.php
+++ index.php
@@ -18,6 +18,29 @@
 
     <?php get_template_part('parts/og-tags'); ?>
     <?php get_template_part('parts/schema'); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+    <script>
+    WebFontConfig = {
+        google: {
+            families: []
+        },
+        typekit: {
+            id: ''
+        },
+        custom: {
+            families: [],
+            urls: ['<?php echo get_template_directory_uri(); ?>/static/fonts/fonts.css']
+        }
+    };
+
+    (function(d) {
+        var wf = d.createElement('script'), s = d.scripts[0];
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+        wf.async = true;
+        s.parentNode.insertBefore(wf, s);
+    })(document);
+    </script>
+
     <?php wp_head();?>
 </head>
 <body <?php body_class(); ?>>
