2da1c2547bedd6ff9272ad73b4c36288649dc070
Sander Moolin <sandermoolin@gmail.com>
2017-10-13 16:10:49 -0400

    WIP on screenshots

--- tests/index.js
+++ tests/index.js
@@ -10,13 +10,16 @@ commander
     .parse(process.argv)
 
 const config = require('../' + commander.config)
-
+const itinerary = config.itinerary || []
+const screenshots = config.screenshots || []
+const dimensions = config.dimensions || [1500]
 const baseUrl = config.baseUrl
 
 console.log(`Testing '${ baseUrl }'...`)
 
 // create output folder with current timestamp
 const outputDir = 'tests/output-' + Date.now()
+let dir = outputDir
 fs.mkdirSync(outputDir)
 
 const puppeteer = require('puppeteer');
@@ -26,92 +29,126 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
-    let index = 1
+    for( let dimension of dimensions ){
 
-    await page.goto(baseUrl)
-    await page.screenshot({ path: outputDir + '/01 - home.png' })
-    console.log(`Step ${ index } - opening '${ baseUrl }'...`)
+        if( typeof dimension == 'number' ){
+            dimension = [dimension, 0]
+        }
 
-    let currentPath = '/'
+        console.log(`Running tests for ${ dimension.join('x') }...`)
 
-    for( let step of config.itinerary ){
-        index++
+        dir = outputDir + '/' + dimension.join('x')
+        fs.mkdirSync(dir)
 
-        let output = `Step ${ index } - `
+        const width = dimension[0]
+        const height = dimension[1]
 
-        switch( step.action ){
+        if( height !== 0 ){
+            await page.setViewport({ width, height })
+        }
 
-        case 'click':
-            output += `clicking on '${ step.selector || 'a' }'...`
-            const target = await page.$eval(step.selector || 'a', el => {
-                return el.getAttribute('href')
-            })
-            currentPath = target
-            await page.click(step.selector || 'a')
-            break
+        let index = 1
 
-        case 'evaluate':
-            // inject as element
-            output += `running function...result:\n`
-            const result = await page.evaluate(func => {
-                return Promise.resolve( eval(func) )
-            }, step.function)
-            output += result
+        let currentPath = '/'
 
-            if( step.expected ){
-                const matches = result == step.expected
-                output += `\nResult of ${ result } ${ matches ? 'matches' : 'does not match' } expected value of ${ step.expected }`
-                output += `${ step.label ? ' ' + step.label : '' }.`
-            }
-            break
+        // Run standard screenshots
+        let i = 1
+        for( let path of screenshots ){
+            await page.goto(baseUrl + '/' + path)
 
-        case 'goto':
-            output += `navigating to ${ step.path }...`
-            currentPath = step.path
-            await page.goto(`${ baseUrl }/${ step.path }`)
-            break
-
-        case 'hover':
-            output += `hovering over '${ step.selector }'...`
-            await page.hover( step.selector )
-            break
-
-        case 'scroll':
-            let num
-            if( step.number ){
-                num = step.number
-            } else {
-                await page.evaluate(selector => {
-                    num = document.querySelector(selector).getBoundingClientRect().top
-                }, [step.selector])
+            if( height === 0 ){
+                let tempHeight = await page.evaluate(() => document.body.scrollHeight)
+                console.log(tempHeight)
+                await page.setViewport({ width, height: tempHeight })
             }
 
-            output += `scrolling ${ step.number ? 'by ' + step.number + 'px' : 'to \'' + step.selector + '\'' }...`
-            await page.evaluate(() => {
-                window.scrollBy(0, num)
-            })
-
-            break
+            await page.screenshot({ path: dir + `/${ i++ } - ${ path.replace(/\//g, '-') }.png` })
+            console.log(`Took screenshot of '/${ path }'...`)
         }
 
-        if( step.screenshot !== false ){
-            output += `screenshot of ${ currentPath }...`
-            let screenshotName = `${ padStart(index, 2, '0') } - `
-            screenshotName += step.label || currentPath.replace(/\//g, ' - ')
-            await page.screenshot({
-                path: outputDir + `/${ screenshotName }.png`
-            })
+        // Run itinerary
+        for( let step of itinerary ){
+            index++
+
+            let output = `Step ${ index } - `
+
+            switch( step.action ){
+
+            case 'click':
+                output += `clicking on '${ step.selector || 'a' }'...`
+                const target = await page.$eval(step.selector || 'a', el => {
+                    return el.getAttribute('href')
+                })
+                currentPath = target
+                await page.click(step.selector || 'a')
+                break
+
+            case 'evaluate':
+                // inject as element
+                output += `running function...result:\n`
+                const result = await page.evaluate(func => {
+                    return Promise.resolve( eval(func) )
+                }, step.function)
+                output += result
+
+                if( step.expected ){
+                    const matches = result == step.expected
+                    output += `\nResult of ${ result } ${ matches ? 'matches' : 'does not match' } expected value of ${ step.expected }`
+                    output += `${ step.label ? ' ' + step.label : '' }.`
+                }
+                break
+
+            case 'goto':
+                output += `navigating to ${ step.path }...`
+                currentPath = step.path
+                await page.goto(`${ baseUrl }/${ step.path }`)
+                break
+
+            case 'hover':
+                output += `hovering over '${ step.selector }'...`
+                await page.hover( step.selector )
+                break
+
+            case 'scroll':
+                let num
+                if( step.number ){
+                    num = step.number
+                } else {
+                    await page.evaluate(selector => {
+                        num = document.querySelector(selector).getBoundingClientRect().top
+                    }, [step.selector])
+                }
+
+                output += `scrolling ${ step.number ? 'by ' + step.number + 'px' : 'to \'' + step.selector + '\'' }...`
+                await page.evaluate(() => {
+                    window.scrollBy(0, num)
+                })
+
+                break
+            }
+
+            if( step.screenshot !== false ){
+                output += `screenshot of ${ currentPath }...`
+                let screenshotName = `${ padStart(index, 2, '0') } - `
+                screenshotName += step.label || currentPath.replace(/\//g, ' - ')
+                await page.screenshot({
+                    path: dir + `/${ screenshotName }.png`
+                })
+            }
+
+            console.log(output)
+
+            if( step.message ){
+                console.log(step.message)
+            }
+            if( step.waitFor == 'load' ){
+                await page.waitForNavigation()
+            }
         }
 
-        console.log(output)
 
-        if( step.message ){
-            console.log(step.message)
-        }
-        if( step.waitFor == 'load' ){
-            await page.waitForNavigation()
-        }
     }
+
 
     await browser.close()
 
