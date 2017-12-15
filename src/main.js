bb7a511fa416d8d3aa836167e400610904d1fbdd
Sander Moolin <sandermoolin@gmail.com>
2017-12-15 10:33:38 -0500

    Added fh-components and commented out ones not used in boilerplate

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import Vue from 'vue'
 import cache from 'src/utils/cache'
 
 // register global components
+// Vue.component('a-div', require('fh-components/a-div').default)
+// Vue.component('flex-text', require('fh-components/flex-text').default)
 Vue.component('responsive-image', require('fh-components/responsive-image').default)
+// Vue.component('video-player', require('fh-components/video-player').default)
+// Vue.component('wp-content', require('fh-components/wp-content').default)
 Vue.component('wp-menu', require('fh-components/wp-menu').default)
 Vue.component('menu-item', require('fh-components/wp-menu-item').default)
 Vue.component('svg-image', require('src/components/SvgImage.vue'))
+
+// Register directives
+// import fullHeight from 'fh-components/v-full-height'
+// Vue.directive('full-height', fullHeight)
 
 // save initial page cache - dumped onto page as `jsonData` by Rest-Easy
 cache[window.location.pathname] = Promise.resolve(jsonData)
