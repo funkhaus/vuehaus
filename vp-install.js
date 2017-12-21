095a523cd233d9155f83f540d1c304b29eed9704
Sander Moolin <sandermoolin@gmail.com>
2017-12-21 12:51:14 -0500

    Fixed install script location

--- vp-install.js
+++ vp-install.js
@@ -3,6 +3,6 @@ const fs = require('fs-extra')
 const colors = require('colors')
 
 console.log('Preparing Vuepress...'.yellow)
-fs.renameSync('./deploy/.deployrc-example', './deploy/.deployrc')
+fs.renameSync('./.deployrc-example', './.deployrc')
 
 console.log('Vuepress installation complete!'.green)
