2aca29e670bfafec6076a075c36f609d54a270b0
Sander Moolin <sandermoolin@gmail.com>
2018-02-01 11:26:52 -0500

    Dev can define list of custom templates and save selected one to a post

---
+++ functions/custom-vuepress-templates.php
@@ -0,0 +1,12 @@
+<?php
+
+    function get_custom_vuepress_templates(){
+
+        return array(
+            'Default',
+            'Test',
+            'Sample',
+            'Test3'
+        );
+
+    }
