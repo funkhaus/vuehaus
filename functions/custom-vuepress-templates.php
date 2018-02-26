5a7169dbc9a33aede15da51f2af8583630fe93ce
Sander Moolin <sandermoolin@gmail.com>
2018-02-26 14:58:34 -0500

    Updated Rest-Easy version

--- functions/custom-vuepress-templates.php
+++ functions/custom-vuepress-templates.php
@@ -4,6 +4,7 @@
 
         // Add Vuepress template names to this array.
         // The user will be able to select any of these templates for any page on the site, overriding the default template.
+        // If a name is in this array, it should exist as a Vue template in src/views as well, using the name as it appears in this list.
         return array(
             'Default'
         );
