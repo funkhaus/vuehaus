8e4a0abada9dedcf913d6b5e5d935f66eda6ebc2
John Robson <john@funkhaus.us>
2018-03-19 15:54:05 -0400

    Added absolute URL support for <svg-image /> component

--- src/components/SvgImage.vue
+++ src/components/SvgImage.vue
@@ -1,20 +1,66 @@
 <template>
-    <div class="svg-wrapper" v-html="svg"></div>
+    <div :class="['svg-wrapper', { loading }]" v-html="innerMarkup" />
 </template>
 
 <script>
     export default {
         template: '',
         props: {
-            src: String
+            src: String,
+            default: ''
+        },
+        data () {
+            return {
+                loadedSVG: '',
+                loading: false
+            }
+        },
+        mounted () {
+            if ( this.src && this.isAbsolute ) this.fetchSVG()
+        },
+        watch: {
+            isAbsolute (absolute) {
+                if ( absolute ) this.fetchSVG()
+            }
         },
         computed: {
-            svg(){
-                if( this.src ) {
-                    return require(`src/svgs/${ this.src }`)
+            isAbsolute () {
+                return Boolean(this.src.match(/^https?:\/\//g))
+            },
+            innerMarkup () {
+                // no src? Stay empty
+                if ( !this.src ) return ''
+
+                // local URL? Attempt to get from webpack
+                if( !this.isAbsolute ) {
+                    try {
+                        return require(`src/svgs/${ this.src }`)
+                    } catch (e) {}
                 }
 
-                return ''
+                // no SVG loaded yet, show IMG tag
+                if ( !this.loadedSVG ) {
+                    return `<img class="svg" src="${ this.src }" />`
+                }
+
+                // return loaded SVG code
+                return this.loadedSVG
+            }
+        },
+        methods: {
+            async fetchSVG () {
+                // set state
+                this.loading = true
+
+                // fetch SVG and validate
+                try {
+                    const response = await fetch(this.src, { credentials: 'same-origin' })
+                    const mimeType = response.headers.get('content-type')
+                    if ( mimeType === 'image/svg+xml' ) {
+                        this.loadedSVG = await response.text()
+                    }
+                } catch (e) {}
+                return this.loading = false
             }
         }
     }
