29b4035cc4c05666ae7060c2badac7b63a1fa51a
Sander Moolin <sandermoolin@gmail.com>
2018-03-29 12:17:20 -0400

    Removed default add_page_siblings filter

--- functions/rest-easy-filters.php
+++ functions/rest-easy-filters.php
@@ -10,25 +10,30 @@
  *
  * @param array $input The post currently being processed by Rest-Easy
  */
-    function add_page_siblings($related, $target = null){
-        $target = get_post($target);
-        $args = array(
-            'posts_per_page'       => -1,
-            'orderby'              => 'menu_order',
-            'order'                => 'ASC',
-            'exclude'              => array( $target->ID ),
-            'post_type'            => 'page',
-            'post_parent'          => $target->post_parent
-        );
-        $siblings = get_posts($args);
 
-        // apply the serialize_object filter to all siblings
-        $related['siblings'] = array_map(
-            function ($sibling) { return apply_filters('rez_serialize_object', $sibling); },
-            $siblings
-        );
+ // NOTE: This is a common Rest-Easy filter, but including it by default can slow
+ //         down load times on large sites. Uncomment to use, but it's best to
+ //         include some extra checks so this doesn't run on every page.
 
-        // return modified data
-        return $related;
-    }
-    add_filter('rez_gather_related', 'add_page_siblings', 10, 2);
+    // function add_page_siblings($related, $target = null){
+    //     $target = get_post($target);
+    //     $args = array(
+    //         'posts_per_page'       => -1,
+    //         'orderby'              => 'menu_order',
+    //         'order'                => 'ASC',
+    //         'exclude'              => array( $target->ID ),
+    //         'post_type'            => 'page',
+    //         'post_parent'          => $target->post_parent
+    //     );
+    //     $siblings = get_posts($args);
+    //
+    //     // apply the serialize_object filter to all siblings
+    //     $related['siblings'] = array_map(
+    //         function ($sibling) { return apply_filters('rez_serialize_object', $sibling); },
+    //         $siblings
+    //     );
+    //
+    //     // return modified data
+    //     return $related;
+    // }
+    // add_filter('rez_gather_related', 'add_page_siblings', 10, 2);
