5f4f664883ed082020b1e4311e3c12cdf13e5223
John Robson <john@funkhaus.us>
2018-04-11 17:50:01 -0400

    upgraded rest-easy, added default filter to add page Dev IDs to nav items

--- functions/rest-easy-filters.php
+++ functions/rest-easy-filters.php
@@ -5,6 +5,28 @@
  * Common examples of filters to add in this file can be found here: https://github.com/funkhaus/vuepress/tree/master/functions
  */
 
+ /**
+  *  Add dev_id to nav items as they are serialized
+  *
+  * @param array $input The nav item currently being processed by Rest-Easy
+  */
+     function add_dev_id_to_nav_items($nav_item_data){
+
+         if ( isset($nav_item_data['id']) ) {
+             // get full nav item
+             $object_type = get_post_meta($nav_item_data['id'], '_menu_item_object', true);
+             $object_id = get_post_meta($nav_item_data['id'], '_menu_item_object_id', true);
+
+             // if nav item is a page, add Dev ID
+             if ( $object_type == 'page' ) {
+                 $nav_item_data['devId'] = get_post_meta($object_id, 'custom_developer_id', true);
+             }
+         }
+
+         return $nav_item_data;
+     }
+     add_filter('rez_serialize_nav_item', 'add_dev_id_to_nav_items');
+
 /**
  *  Serialize page siblings
  *
