e26ce82c8ec1e40fe72a2247b2ade54f80da3bb9
Sander <sander@funkhaus.us>
2018-08-20 11:16:29 -0400

    Snapshot of more advanced component WIP

--- blocks/dist/blocks.js
+++ blocks/dist/blocks.js
@@ -1,53 +1,133 @@
-!(function(e) {
-    function t(r) {
-        if (n[r]) return n[r].exports
-        var o = (n[r] = { i: r, l: !1, exports: {} })
-        return e[r].call(o.exports, o, o.exports, t), (o.l = !0), o.exports
-    }
-    var n = {}
-    ;(t.m = e),
-        (t.c = n),
-        (t.d = function(e, n, r) {
-            t.o(e, n) ||
-                Object.defineProperty(e, n, {
-                    configurable: !1,
-                    enumerable: !0,
-                    get: r
-                })
-        }),
-        (t.n = function(e) {
-            var n =
-                e && e.__esModule
-                    ? function() {
-                          return e.default
-                      }
-                    : function() {
-                          return e
-                      }
-            return t.d(n, 'a', n), n
-        }),
-        (t.o = function(e, t) {
-            return Object.prototype.hasOwnProperty.call(e, t)
-        }),
-        (t.p = ''),
-        t((t.s = 0))
-})([
-    function(e, t) {
-        ;(0, wp.blocks.registerBlockType)('custom-fh/template-block', {
-            title: 'Worker Block',
-            description: 'The block template.',
-            icon: 'universal-access-alt',
-            category: 'custom-fh',
-            edit: function() {
-                return wp.element.createElement('p', null, 'Hello editor.')
-            },
-            save: function() {
-                return wp.element.createElement(
-                    'p',
-                    null,
-                    'Hello saved content.'
-                )
-            }
-        })
-    }
-])
+/******/ ;(function(modules) {
+    // webpackBootstrap
+    /******/ // The module cache
+    /******/ var installedModules = {} // The require function
+    /******/
+    /******/ /******/ function __webpack_require__(moduleId) {
+        /******/
+        /******/ // Check if module is in cache
+        /******/ if (installedModules[moduleId]) {
+            /******/ return installedModules[moduleId].exports
+            /******/
+        } // Create a new module (and put it into the cache)
+        /******/ /******/ var module = (installedModules[moduleId] = {
+            /******/ i: moduleId,
+            /******/ l: false,
+            /******/ exports: {}
+            /******/
+        }) // Execute the module function
+        /******/
+        /******/ /******/ modules[moduleId].call(
+            module.exports,
+            module,
+            module.exports,
+            __webpack_require__
+        ) // Flag the module as loaded
+        /******/
+        /******/ /******/ module.l = true // Return the exports of the module
+        /******/
+        /******/ /******/ return module.exports
+        /******/
+    } // expose the modules object (__webpack_modules__)
+    /******/
+    /******/
+    /******/ /******/ __webpack_require__.m = modules // expose the module cache
+    /******/
+    /******/ /******/ __webpack_require__.c = installedModules // define getter function for harmony exports
+    /******/
+    /******/ /******/ __webpack_require__.d = function(exports, name, getter) {
+        /******/ if (!__webpack_require__.o(exports, name)) {
+            /******/ Object.defineProperty(exports, name, {
+                /******/ configurable: false,
+                /******/ enumerable: true,
+                /******/ get: getter
+                /******/
+            })
+            /******/
+        }
+        /******/
+    } // getDefaultExport function for compatibility with non-harmony modules
+    /******/
+    /******/ /******/ __webpack_require__.n = function(module) {
+        /******/ var getter =
+            module && module.__esModule
+                ? /******/ function getDefault() {
+                      return module['default']
+                  }
+                : /******/ function getModuleExports() {
+                      return module
+                  }
+        /******/ __webpack_require__.d(getter, 'a', getter)
+        /******/ return getter
+        /******/
+    } // Object.prototype.hasOwnProperty.call
+    /******/
+    /******/ /******/ __webpack_require__.o = function(object, property) {
+        return Object.prototype.hasOwnProperty.call(object, property)
+    } // __webpack_public_path__
+    /******/
+    /******/ /******/ __webpack_require__.p = '' // Load entry module and return exports
+    /******/
+    /******/ /******/ return __webpack_require__((__webpack_require__.s = 0))
+    /******/
+})(
+    /************************************************************************/
+    /******/ [
+        /* 0 */
+        /***/ function(module, exports) {
+            var registerBlockType = wp.blocks.registerBlockType
+            var RichText = wp.editor.RichText
+
+            registerBlockType('custom-fh/block-slug', {
+                title: 'Worker',
+                description: 'The current block',
+                icon: 'universal-access-alt',
+                category: 'custom-fh',
+
+                // This defines the shape of the data in the functions below
+                attributes: {
+                    content: {
+                        type: 'array',
+                        source: 'children',
+                        selector: 'p'
+                    }
+                },
+
+                // Editor
+                edit: function edit(_ref) {
+                    var attributes = _ref.attributes,
+                        className = _ref.className,
+                        setAttributes = _ref.setAttributes
+                    var content = attributes.content
+
+                    function onChangeContent(newContent) {
+                        setAttributes({ content: newContent })
+                    }
+
+                    return wp.element.createElement(RichText, {
+                        tagName: 'p',
+                        className: className,
+                        onChange: onChangeContent,
+                        value: content
+                    })
+                },
+
+                // On save
+                save: function save(_ref2) {
+                    var attributes = _ref2.attributes,
+                        className = _ref2.className
+                    var content = attributes.content
+
+                    return wp.element.createElement(RichText.Content, {
+                        tagName: 'p',
+                        className: className,
+                        value: content
+                    })
+                }
+            })
+
+            /***/
+        }
+        /******/
+    ]
+)
--- blocks/src/index.js
+++ blocks/src/index.js
@@ -1,16 +1,49 @@
 const { registerBlockType } = wp.blocks
+const { RichText } = wp.editor
 
-registerBlockType('custom-fh/template-block', {
-    title: 'Worker Block',
-    description: 'The block template.',
+registerBlockType('custom-fh/block-slug', {
+    title: 'Worker',
+    description: 'The current block',
     icon: 'universal-access-alt',
     category: 'custom-fh',
 
-    edit() {
-        return <p>Hello editor.</p>
+    // This defines the shape of the data in the functions below
+    attributes: {
+        content: {
+            type: 'array',
+            source: 'children',
+            selector: 'p'
+        }
     },
 
-    save() {
-        return <p>Hello saved content.</p>
+    // Editor
+    edit({ attributes, className, setAttributes }) {
+        const { content } = attributes
+
+        function onChangeContent(newContent) {
+            setAttributes({ content: newContent })
+        }
+
+        return (
+            <RichText
+                tagName="p"
+                className={className}
+                onChange={onChangeContent}
+                value={content}
+            />
+        )
+    },
+
+    // On save
+    save({ attributes, className }) {
+        const { content } = attributes
+
+        return (
+            <RichText.Content
+                tagName="p"
+                className={className}
+                value={content}
+            />
+        )
     }
 })
